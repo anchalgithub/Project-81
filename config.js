62d802cba2f4c9f5f466e3175ebbac7dcca4e0be
ASinha <69646570+anchalgithub@users.noreply.github.com>
2021-02-05 17:10:42 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+require('@firebase/firestore')
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBVdqWajEpMrgN8DcdqhbdUK6AkbGP-g8E",
+  authDomain: "bartar-system.firebaseapp.com",
+  databaseURL: "https://bartar-system.firebaseio.com",
+  projectId: "bartar-system",
+  storageBucket: "bartar-system.appspot.com",
+  messagingSenderId: "573044276501",
+  appId: "1:573044276501:web:f6f2ef6db00ef2683efd1c",
+  measurementId: "G-YWPCLZGHR3"
+};
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase.firestore();
+
+  
